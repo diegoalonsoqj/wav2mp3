e9398ed801f8b05f10f25d875162850da414da1a
Diego Quispe <72992112+diegoalonsoqj@users.noreply.github.com>
2020-10-16 12:16:56 -0500

    Script base
    Conteo de archivos .WAV.
    Conteo de archivos .MP3.
    Conversión de formato de archivos de audio.
    Mensaje final por consola sobre el resultado del procesamiento de archivos.

---
+++ wav2mp3.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+mkdir formato_mp3
+
+DirRaiz=$(pwd)
+PesoWav=$(du -hs)
+
+timeInit=$(date +"%d/%m/%Y %T")
+
+let TotalAudios=$(find . -maxdepth 1 -type f -iname "*.wav" | wc -l)
+
+for i in *.wav
+do
+    echo PROCESANDO $i
+    lame -b 16 -m m -q 9 --resample 8 "$i" "formato_mp3/${i/.wav/.mp3}"
+done
+
+
+let TotalWav=$(find . -maxdepth 1 -type f -iname "*.wav" | wc -l)
+echo ""
+echo "ARCHIVOS WAV: "
+echo "- Cantidad total de audios WAV: " $TotalWav
+echo "- Peso de los audios WAV: " $PesoWav
+
+echo ""
+cd formato_mp3
+
+PesoMp3=$(du -hs)
+
+let TotalMp3=$(find . -maxdepth 1 -type f -iname "*.mp3" | wc -l)
+echo "ARCHIVOS MP3: "
+echo "- Cantidad total de audios MP3: " $TotalMp3
+echo "- Peso de los audios MP3: " $PesoMp3
+
+let difWavMp3=$TotalWav-$TotalMp3
+
+timeFin=$(date +"%d/%m/%Y %T")
+
+if [ $TotalWav -ne $TotalMp3 ]; then
+	echo ""
+	echo " ERROR - Existe un diferencia de cantidades"
+	echo "- Diferencia de cantidades de archivos entre WAV y MP3: " $difWavMp3
+else
+	echo ""
+        echo " CONVERSION DE AUDIOS EXITOSA !"
+	echo "- Fecha y Hora de Inicio: " $timeInit
+	echo "- Fecha y Hora de Fin: " $timeFin
+       	echo "- Diferencia de cantidades de archivos entre WAV y MP3: " $difWavMp3
+fi
