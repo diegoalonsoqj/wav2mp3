8c4540771c4873119117b45aa48f418397f186a3
Diego Quispe <72992112+diegoalonsoqj@users.noreply.github.com>
2023-07-05 22:59:21 -0500

    v2.0
    Mensajes y validaciones optimizadas.

--- wav2mp3.sh
+++ wav2mp3.sh
@@ -3,11 +3,26 @@
 # Script by Diego Quispe - da@diegoquispe.com
 # Lima - Peru
 
+if ! command -v lame &> /dev/null
+then
+    echo "lame no está instalado. Por favor, instálalo para continuar."
+    exit
+fi
+
+if [ -d "formato_mp3" ]; then
+  echo "El directorio 'formato_mp3' ya existe. ¿Quieres eliminarlo? (S/N)"
+  read respuesta
+  if [[ $respuesta =~ ^[Ss]$ ]]
+  then
+      rm -rf formato_mp3
+  else
+      exit 1
+  fi
+fi
+
 mkdir formato_mp3
 
 DirRaiz=$(pwd)
-PesoWav=$(du -hs)
-
 timeInit=$(date +"%d/%m/%Y %T")
 
 let TotalAudios=$(find . -maxdepth 1 -type f -iname "*.wav" | wc -l)
@@ -18,6 +33,7 @@ do
     lame -b 16 -m m -q 9 --resample 8 "$i" "formato_mp3/${i/.wav/.mp3}"
 done
 
+PesoWav=$(du -hs)
 
 let TotalWav=$(find . -maxdepth 1 -type f -iname "*.wav" | wc -l)
 echo ""
@@ -31,22 +47,4 @@ cd formato_mp3
 PesoMp3=$(du -hs)
 
 let TotalMp3=$(find . -maxdepth 1 -type f -iname "*.mp3" | wc -l)
-echo "ARCHIVOS MP3: "
-echo "- Cantidad total de audios MP3: " $TotalMp3
-echo "- Peso de los audios MP3: " $PesoMp3
-
-let difWavMp3=$TotalWav-$TotalMp3
-
-timeFin=$(date +"%d/%m/%Y %T")
-
-if [ $TotalWav -ne $TotalMp3 ]; then
-	echo ""
-	echo " ERROR - Existe un diferencia de cantidades"
-	echo "- Diferencia de cantidades de archivos entre WAV y MP3: " $difWavMp3
-else
-	echo ""
-        echo " CONVERSION DE AUDIOS EXITOSA !"
-	echo "- Fecha y Hora de Inicio: " $timeInit
-	echo "- Fecha y Hora de Fin: " $timeFin
-       	echo "- Diferencia de cantidades de archivos entre WAV y MP3: " $difWavMp3
-fi
+echo "
